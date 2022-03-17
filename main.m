caf8b263d0985e2e134766962b70d8f0e1d570d1
Robin Onsay <hello@robinonsay.com>
2022-03-17 18:08:05 -0400

    simulates QPSK with Root Raised Cosine filter over AWGN channel

--- main.m
+++ main.m
@@ -19,15 +19,16 @@ rx = rxsite("CoordinateSystem", "cartesian", "AntennaPosition", [config.Target_D
 signalStrength = sigstrength(rx, tx, "freespace");
 fprintf("Signal Strength at Receiver: %d dBm\n", signalStrength);
 noiseFloorVec = (-120:0.5:-90)';
+minEbNo = config.Receiver_Sensitivity - noiseFloorVec(end) - ...
+    10*log10(config.Target_Data_Rate/config.Bandwidth);
 snrVec = signalStrength - noiseFloorVec;
 EbNoVec = snrVec - 10*log10(config.Target_Data_Rate/config.Bandwidth);
 channel = comm.AWGNChannel("VarianceSource", "Input port", ...
     "NoiseMethod", "Variance");
 bitErrRate = comm.ErrorRate("ResetInputPort", true);
-symErrRate = comm.ErrorRate("ResetInputPort", true);
 % snrVec = EbNoVec + 10*log10(config.Target_Data_Rate/config.Bandwidth);
 berVec = zeros(length(noiseFloorVec),3);
-serVec = zeros(length(noiseFloorVec),3);
+serVec = zeros(length(noiseFloorVec),2);
 dataIn = randi([0,1], 1e6, 1);
 %% Simulation
 if contains(config.Mod_Scheme, "QPSK")
@@ -41,13 +42,15 @@ if contains(config.Mod_Scheme, "QPSK")
         snr = snrVec(i);
         qpskTx = qpskMod(dataIn);
         txSig = txfilter(qpskTx);
+%         txSig = qpskTx;
         powerDB = 10*log10(var(txSig));
         noiseVar = 10.^(0.1*(powerDB-snr));
         rxSig = channel(txSig, noiseVar);
         qpskRx = rxfilter(rxSig);
+%         qpskRx = rxSig;
         dataOut = qpskDemod(qpskRx);
         berVec(i,:) = bitErrRate(dataIn, dataOut, 1);
-        serVec(i,:) = symErrRate(qpskTx, qpskRx, 1);
+        serVec(i,:) = symerr(qpskTx, qpskRx);
     end
     berTheory = berawgn(EbNoVec, 'psk', M, 'nondiff');
 end
@@ -55,6 +58,8 @@ figure
 semilogy(EbNoVec,berVec(:,1),'*')
 hold on
 semilogy(EbNoVec,berTheory)
+xline(minEbNo);
+ylim([1e-1, 1e1]);
 title('BER');
 legend('Simulation','Theory','Location','Best')
 xlabel('Eb/No (dB)')
@@ -63,7 +68,7 @@ grid on
 hold off
 
 figure
-semilogy(EbNoVec,serVec(:,1),'*')
+semilogy(EbNoVec,serVec(:,2),'*')
 hold on
 title('SER');
 xlabel('Eb/No (dB)')
