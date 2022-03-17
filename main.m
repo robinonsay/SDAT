410432a24cb87872bcf7de6adab1acf1c4c181df
Robin Onsay <hello@robinonsay.com>
2022-03-17 16:22:48 -0400

    Calculates BER based on noise variance from EbNo

--- main.m
+++ main.m
@@ -1,3 +1,6 @@
+%% Constants
+SEED = 3165;
+%% Import Config File
 config_name = input("Config File Name? ", "s");
 if isempty(config_name)
     config_name = "default_config.json";
@@ -6,45 +9,32 @@ fid = fopen(config_name);
 config_json = char(fread(fid, inf)');
 fclose(fid);
 config = jsondecode(config_json);
-M = 4;                 % Modulation alphabet
-k = log2(M);           % Bits/symbol
-numSC = 128;           % Number of OFDM subcarriers
-cpLen = 32;            % OFDM cyclic prefix length
-maxBitErrors = 100;    % Maximum number of bit errors
-maxNumBits = 1e7;      % Maximum number of bits transmitted
-qpskMod = comm.QPSKModulator('BitInput',true);
-qpskDemod = comm.QPSKDemodulator('BitOutput',true);
-ofdmMod = comm.OFDMModulator('FFTLength',numSC,'CyclicPrefixLength',cpLen);
-ofdmDemod = comm.OFDMDemodulator('FFTLength',numSC,'CyclicPrefixLength',cpLen);
-channel = comm.AWGNChannel('NoiseMethod','Variance', ...
-    'VarianceSource','Input port');
-errorRate = comm.ErrorRate('ResetInputPort',true);
-ofdmDims = info(ofdmMod)
-numDC = ofdmDims.DataInputSize(1)
-frameSize = [k*numDC 1];
-EbNoVec = (0:10)';
-snrVec = EbNoVec + 10*log10(k) + 10*log10(numDC/numSC);
+%% Setup
+rng(SEED);
+channel = comm.AWGNChannel("VarianceSource", "Input port", ...
+    "NoiseMethod", "Variance");
+errorRate = comm.ErrorRate("ResetInputPort", true);
+EbNoVec = (-45:-30)';
+snrVec = EbNoVec + 10*log10(config.Target_Data_Rate/config.Bandwidth);
 berVec = zeros(length(EbNoVec),3);
-errorStats = zeros(1,3);
-for m = 1:length(EbNoVec)
-    snr = snrVec(m);
-    
-    while errorStats(2) <= maxBitErrors && errorStats(3) <= maxNumBits
-        dataIn = randi([0,1],frameSize);              % Generate binary data
-        qpskTx = qpskMod(dataIn);                     % Apply QPSK modulation
-        txSig = ofdmMod(qpskTx);                      % Apply OFDM modulation
-        powerDB = 10*log10(var(txSig));               % Calculate Tx signal power
-        noiseVar = 10.^(0.1*(powerDB-snr));           % Calculate the noise variance
-        rxSig = channel(txSig,noiseVar);              % Pass the signal through a noisy channel
-        qpskRx = ofdmDemod(rxSig);                    % Apply OFDM demodulation
-        dataOut = qpskDemod(qpskRx);                  % Apply QPSK demodulation
-        errorStats = errorRate(dataIn,dataOut,0);     % Collect error statistics
+dataIn = randi([0,1], 1e6, 1);
+%% Simulation
+if contains(config.Mod_Scheme, "QPSK")
+    M = 4;
+    k = log2(M);
+    qpskMod = comm.QPSKModulator('BitInput',true);
+    qpskDemod = comm.QPSKDemodulator('BitOutput',true);
+    for i = 1:length(EbNoVec)
+        snr = snrVec(i);
+        txSig = qpskMod(dataIn);
+        powerDB = 10*log10(var(txSig));
+        noiseVar = 10.^(0.1*(powerDB-snr));
+        rxSig = channel(txSig, noiseVar);
+        dataOut = qpskDemod(rxSig);
+        berVec(i,:) = errorRate(dataIn, dataOut, 1);
     end
-    
-    berVec(m,:) = errorStats;                         % Save BER data
-    errorStats = errorRate(dataIn,dataOut,1);         % Reset the error rate calculator
+    berTheory = berawgn(EbNoVec,'psk',M,'nondiff');
 end
-berTheory = berawgn(EbNoVec,'psk',M,'nondiff');
 figure
 semilogy(EbNoVec,berVec(:,1),'*')
 hold on
